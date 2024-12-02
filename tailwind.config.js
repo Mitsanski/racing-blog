10d8f861ecf00b03a8751a5c8a367e297fed4f1e
Kiril Mitsanski <mitsanskikiril@outlook.com>
2024-12-02 21:02:03 +0200

    added tailwind to the project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,js,cjs,mjs,ts,cts,mts}", // Include all Angular HTML and TypeScript files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
